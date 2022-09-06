ac5437426809d72413aec8892b95103071e688e5
AxelBouvierM <axelbouvierm@gmail.com>
2022-09-06 18:51:56 +0000

    extend your Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,8 +8,8 @@ from sys import argv
 
 if __name__ == "__main__":
     api = "https://jsonplaceholder.typicode.com/"
-    api_url_usr = api + "users/" + int(argv[1])
-    api_url_todos = api + "todos?userId=" + int(argv[1])
+    api_url_usr = api + "users/" + argv[1]
+    api_url_todos = api + "todos?userId=" + argv[1]
 
     response = requests.get(api_url_usr).json()
     todo = requests.get(api_url_todos).json()
