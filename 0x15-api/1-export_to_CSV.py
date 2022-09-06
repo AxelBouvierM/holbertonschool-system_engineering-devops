3f635d5309f7c2002c95dcfae001dd0013cebb5e
AxelBouvierM <axelbouvierm@gmail.com>
2022-09-06 18:51:33 +0000

    extend your Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,13 +8,13 @@ from sys import argv
 
 if __name__ == "__main__":
     api = "https://jsonplaceholder.typicode.com/"
-    api_url_usr = api + "users/" + argv[1]
-    api_url_todos = api + "todos?userId=" + argv[1]
+    api_url_usr = api + "users/" + int(argv[1])
+    api_url_todos = api + "todos?userId=" + int(argv[1])
 
     response = requests.get(api_url_usr).json()
     todo = requests.get(api_url_todos).json()
 
-    filename = todo.get("userId") + ".csv"
+    filename = argv[1] + ".csv"
     with open(filename, 'w', newline='') as csvfile:
         write_file = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
